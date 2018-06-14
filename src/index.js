26d970ca1284a9cdeac17c2a8e7fad856755a56c
Joey Rozier <mrjoro@users.noreply.github.com>
2018-06-14 14:09:37 -0700

    remove tinyspeck in favor of slacks node apis

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
-// Initialize using verification token from environment variables
+// Use dotenv to read the local .env file during development
+// (otherwise the process.env comes from the process running
+// this, e.g. Heroku config vars)
 const dotenv = require('dotenv')
 const ENV = process.env.NODE_ENV || 'development'
 if (ENV === 'development') dotenv.load()
@@ -7,15 +9,16 @@ const createSlackEventAdapter = require('@slack/events-api').createSlackEventAda
 const slackEvents = createSlackEventAdapter(process.env.SLACK_VERIFICATION_TOKEN);
 const port = process.env.PORT || 3000;
 
-// Attach listeners to events by Slack Event "type". See: https://api.slack.com/events/message.im
-slackEvents.on('message', (event) => {
-  console.log(`Received a message event: user ${event.user} in channel ${event.channel} says ${event.text}`);
-});
-
 slackEvents.on('reaction_added', event => {
   let {type, user, item} = event;
 
   console.log(`a reaction was added by ${user}: ${JSON.stringify(item)}`);
+});
+
+slackEvents.on('team_join', event => {
+  let {type, user} = event;
+
+  console.log(`the following user joined the team: ${JSON.stringify(user)})`);
 });
 
 slackEvents.on('member_joined_channel', event => {
@@ -29,32 +32,5 @@ slackEvents.on('error', console.error);
 
 // Start a basic HTTP server
 slackEvents.start(port).then(() => {
-  console.log(`server listening for events on port ${port}`);
+  console.log(`server listening for events on port ${port} at /slack/events`);
 });
-
-/*
-let slack = ts.instance(
-  { token: TOKEN });
-
-slack.on('reaction_added', payload => {
-  let {type, user, item} = payload.event;
-
-  console.log(`a reaction was added by ${user}: ${JSON.stringify(item)}`);
-});
-
-slack.on('team_join', payload => {
-  let {type, user} = payload.event;
-
-  console.log(`the following user joined the team: ${JSON.stringify(user)})`);
-});
-
-slack.on('member_joined_channel', payload => {
-  let {type, user, channel} = payload.event;
-
-  console.log(`member ${user} joined ${channel})`);
-});
-
-
-// incoming http requests
-slack.listen(PORT);
-*/
