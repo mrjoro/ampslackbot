891c3cd0063485f2aa112885c98133e49f14948a
Joey Rozier <mrjoro@users.noreply.github.com>
2018-06-14 09:22:11 -0700

    improve logging, handle member joined

--- src/index.js
+++ src/index.js
@@ -4,11 +4,22 @@ const TOKEN = process.env.TOKEN;
 
 let slack = ts.instance(/*{ token: TOKEN }*/);
 
-// event handler
-slack.on('team_joined', 'reaction_added', payload => {
+slack.on('reaction_added', payload => {
   let {type, user, item} = payload.event;
 
-  console.log(`event handler called ${type}, ${user}, ${item}`);
+  console.log(`a reaction was added by ${user}: ${JSON.stringify(item)}`);
+});
+
+slack.on('team_joined', payload => {
+  let {type, user} = payload.event;
+
+  console.log(`the following user joined the team: ${JSON.stringify(user)})`);
+});
+
+slack.on('member_joined_channel', payload => {
+  let {type, user, channel} = payload.event;
+
+  console.log(`member ${user} joined ${channel})`);
 });
 
 
